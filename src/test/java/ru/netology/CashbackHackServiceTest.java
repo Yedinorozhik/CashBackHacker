d507936a1bc557ce6725aea65471c0fb3d2be7c6
Alyona Yedinorozhik <misterrpandaa@gmail.com>
2025-06-17 12:09:05 +0300

    TestNG

---
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -0,0 +1,48 @@
+package ru.netology;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void notEnoughForCashback () {
+
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 900;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 100;
+
+       assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void enoughForCashback () {
+
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void MoreEnoughForCashback () {
+
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1100;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+
+    }
+
+}
